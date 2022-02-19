3f87ab1e0ca9bca29a1e4415355fb4f205a4e298
Hector Siman <hectorlutero@hotmail.com>
2022-02-19 13:53:23 -0300

    new(): Backend server setup and First Get routes

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const products = require("./data/products");
+
+const app = express();
+const port = 5000;
+
+// ROUTES
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/api/products", (req, res) => res.json(products));
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find(p => p._id === req.params.id);
+
+  res.json(product);
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}!`));
