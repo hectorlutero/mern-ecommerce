7e21e0a7aa7b18f7ac54de0d3633b1021ac937a6
Hector Siman <hectorlutero@hotmail.com>
2022-02-20 00:17:24 -0300

    new(): Routes Fetching products from the DB Setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
 import colors from "colors";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -12,13 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // ROUTES
-app.get("/", (req, res) => res.send("Hello World!"));
-app.get("/api/products", (req, res) => res.json(products));
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find(p => p._id === req.params.id);
-
-  res.json(product);
-});
+app.get("/", (req, res) => res.send("API is runnning!"));
+app.use("/api/products", productRoutes);
+// app.get("/api/products", (req, res) => res.json(products));
+// app.get("/api/products/:id", (req, res) => {
+//   const product = products.find(p => p._id === req.params.id);
+//   res.json(product);
+// });
 
 app.listen(PORT, () =>
   console.log(
