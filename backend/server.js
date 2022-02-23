3285adacf22842a649906ba30f6429c7428d6f94
Hector Siman <hectorlutero@hotmail.com>
2022-02-23 15:56:51 -0300

    new(): Creating Authentication Endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMidddleware.js";
 
 dotenv.config();
@@ -10,11 +11,15 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
 // ROUTES
 app.get("/", (req, res) => res.send("API is runnning!"));
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 // MIDDLEWARES
 app.use(notFound);
